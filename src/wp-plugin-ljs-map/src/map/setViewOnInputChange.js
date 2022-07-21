5187d185995d7b2a19dc40dad0a70560e4db2293
tlmn <mail@tlmn.io>
2022-07-21 15:06:26 +0200

    use offset

--- src/wp-plugin-ljs-map/src/map/setViewOnInputChange.js
+++ src/wp-plugin-ljs-map/src/map/setViewOnInputChange.js
@@ -6,15 +6,20 @@ import { useMap } from "react-leaflet";
 const SetViewOnInputChange = () => {
   const {
     appState: {
-      map: { center },
+      map: { bounds },
     },
   } = useAppContext();
 
   const map = useMap();
 
   useEffect(() => {
-    map.flyTo(center, 8);
-  }, [center]);
+    let offset= 0.5
+    let newBounds = [
+      [bounds[0][0] + offset, bounds[0][1] - offset],
+      [bounds[1][0] - offset, bounds[1][1] + offset],
+    ];
+    map.flyToBounds(newBounds);
+  }, [bounds, map]);
 };
 
 export default SetViewOnInputChange;
