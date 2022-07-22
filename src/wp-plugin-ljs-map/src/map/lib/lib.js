3d08af595dc4bed436a3fbe8c56f4343961f3ea0
tlmn <mail@tlmn.io>
2022-07-22 15:31:45 +0200

    load sample data and fix var redefinition

--- src/wp-plugin-ljs-map/src/map/lib/lib.js
+++ src/wp-plugin-ljs-map/src/map/lib/lib.js
@@ -1,15 +1,19 @@
-import axios from "axios";
+// import axios from "axios";
+import { ENTITIES } from "../../data/sampleData";
 
 export const calcDistance = (lat1, lon1, lat2, lon2) => {
   var R = 6371;
   var dLat = toRad(lat2 - lat1);
   var dLon = toRad(lon2 - lon1);
-  var lat1 = toRad(lat1);
-  var lat2 = toRad(lat2);
+  var radLat1 = toRad(lat1);
+  var radLat2 = toRad(lat2);
 
   var a =
     Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-    Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
+    Math.sin(dLon / 2) *
+      Math.sin(dLon / 2) *
+      Math.cos(radLat1) *
+      Math.cos(radLat2);
   var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
   var d = R * c;
   return d;
@@ -18,7 +22,7 @@ export const calcDistance = (lat1, lon1, lat2, lon2) => {
 const toRad = (value) => (value * Math.PI) / 180;
 
 export const getEntities = async () => {
-  const res = await axios.get(
+  /*  const res = await axios.get(
     "https://login.stage.linksjugend-solid.de/api/v1/map/getorgunits",
     {},
     {
@@ -29,5 +33,7 @@ export const getEntities = async () => {
     }
   );
 
-  return res?.data ? res.data : {};
+  return res?.data ? res.data : {}; */
+
+  return ENTITIES;
 };
